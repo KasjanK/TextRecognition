b2ce60908e2efa3009e2926ce39be4f01880bd83
KasjanK <kasol99@gmail.com>
2021-11-02 17:31:28 +0100

    MS4: UI: Better looking UI

--- app/src/main/java/com/example/textrecognition/ImageScanner.java
+++ app/src/main/java/com/example/textrecognition/ImageScanner.java
@@ -1,24 +1,21 @@
 package com.example.textrecognition;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Point;
-import android.graphics.Rect;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
@@ -35,8 +32,6 @@ public class ImageScanner extends AppCompatActivity {
     private TextView resultTV;
     private ImageView imageView;
     private Uri uri;
-    int SELECT_IMAGE_CODE = 1;
-
     private TextToSpeech textToSpeech;
 
     @Override
@@ -44,53 +39,41 @@ public class ImageScanner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_scanner);
 
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-            }
-        });
-
         btn = findViewById(R.id.btn);
         imageView = findViewById(R.id.pickedImage);
         resultTV = findViewById(R.id.detectedText);
         detectBtn = findViewById(R.id.detectBtn);
 
-        detectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                detectText();
-            }
+        textToSpeech = new TextToSpeech(this, status -> {
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        detectBtn.setOnClickListener(view -> detectText());
 
-            // välj bild-menyn
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Title"), SELECT_IMAGE_CODE);
-            }
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            getContent.launch(intent);
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1) {
-            uri = data.getData();
-            imageView.setImageURI(uri);
+    ActivityResultLauncher<Intent> getContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                uri = data.getData();
+                imageView.setImageURI(uri);
+            }
         }
-    }
+    });
 
-    // känner igen text från bilden
+    // recognizing and saying the text from images
     private void detectText() {
-        // implementerar en textrecognizer från googles api
+        // textrecognizer from google's api
         TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
 
-        // förbereder den valda bilden
+        // preparing the chosen image
         InputImage image = null;
         try {
             image = InputImage.fromFilePath(this, uri);
@@ -98,43 +81,28 @@ public class ImageScanner extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        // bearbetar bilden
-        Task<Text> result = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
-            @Override
-            // om den lyckas, så extraheras text ur bilden
-            public void onSuccess(@NonNull Text text) {
-                StringBuilder result = new StringBuilder();
-                for (Text.TextBlock block: text.getTextBlocks()) {
-                    String blockText = block.getText();
-                    Point[] blockCornerPoint = block.getCornerPoints();
-                    Rect blockFrame = block.getBoundingBox();
-                    for (Text.Line line : block.getLines()) {
-                        String lineText = line.getText();
-                        Point[] lineCornerPoint = line.getCornerPoints();
-                        Rect lineFrame = line.getBoundingBox();
-                        for (Text.Element element : line.getElements()) {
-                            String elementText = element.getText();
-                            Point[] elementCornerPoints = element.getCornerPoints();
-                            Rect elementFrame = element.getBoundingBox();
-                            result.append(elementText);
-                        }
-                        resultTV.setText(blockText);
-                        // making the block to string so the tts can recognize it
-                        String words = resultTV.getText().toString();
-                        // making tts say the text
-                        textToSpeech.speak(words, TextToSpeech.QUEUE_FLUSH, null, null);
-                    }
-                }
+        // processing of image
+        Task<Text> result = recognizer.process(image).addOnSuccessListener(text -> {
+            for (Text.TextBlock block: text.getTextBlocks()) {
+                String blockText = block.getText();
+                resultTV.setText(blockText);
+
+                // converting the block to string so the tts can recognize it
+                String words = resultTV.getText().toString();
+
+                // making tts say the text
+                textToSpeech.speak(words, TextToSpeech.QUEUE_FLUSH, null, null);
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            // om appen misslyckas så får man ett meddelande att nåt är fel
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ImageScanner.this, "failed to detect text from image", Toast.LENGTH_SHORT).show();
+            // if the app fail, show a message
+        }).addOnFailureListener(e -> Toast.makeText(ImageScanner.this, "failed to detect text from image", Toast.LENGTH_SHORT).show());
+    }
 
-            }
-        });
-
-
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }
